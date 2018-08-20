21c89fc8e16917ff3674aa0797c5e09112574424
Durgamahesh <durgamahesh888@gmail.com>
2018-08-20 09:23:21 +0530

    First Python scripts are Added.

---
+++ source/heloworld.py
@@ -0,0 +1,5 @@
+a= 19
+if a<=18:
+    print("Hello Wolrd")
+else:
+    print("Welcome to Wolrd")
---
+++ source/varaibles.py
@@ -0,0 +1,19 @@
+""" Variables Partise """
+# alt + Insert  -> to See option for module creation
+# shift + alt + f10  --> to see execution option
+# shift + f10  ---> to return the previous execution
+
+emp_name = "xyz" # string
+emp_id = 1234    # Integer
+emp_email = "xyz@gmail.com"  # string
+emp_salary = 23456.78  #float
+emp_address = "22/A, Street road" \
+              "Hyderabad" \
+              "India"
+emp_phone = 9704878431
+
+system_ip = "10.3.2.6"
+
+print(emp_name, emp_id, emp_phone,system_ip)
+
+
